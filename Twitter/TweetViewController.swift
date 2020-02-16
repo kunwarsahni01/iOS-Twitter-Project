695f6353ded50fa0979711f0f0acecc4ef0651f3
Kunwar Sahni <kunwar.sahni01@gmail.com>
2020-02-16 13:52:32 -0500

    Added Tweet Composition Screen

---
+++ Twitter/TweetViewController.swift
@@ -0,0 +1,32 @@
+//
+//  TweetViewController.swift
+//  Twitter
+//
+//  Created by Kunwar Sahni on 2/16/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetViewController: UIViewController {
+
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+    @IBOutlet weak var tweetButton: UIBarButtonItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
