364699104051b964310013336e2b4251a649cde9
Kunwar Sahni <kunwar.sahni01@gmail.com>
2020-02-16 15:07:02 -0500

    Added Retweet Functionality

--- Twitter/HomeTableTableViewController.swift
+++ Twitter/HomeTableTableViewController.swift
@@ -107,6 +107,10 @@ class HomeTableTableViewController: UITableViewController {
             cell.profileImage.image = UIImage(data: imageData)
         }
         
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
+        
         return cell
     }
 
--- Twitter/TableViewCell.swift
+++ Twitter/TableViewCell.swift
@@ -18,15 +18,52 @@ class TableViewCell: UITableViewCell {
         super.awakeFromNib()
         // Initialization code
     }
+    var tweetId:Int = -1
+    var favorited:Bool = false
+    var retweeted:Bool = false
 
-    
-    
     @IBAction func retweetTapped(_ sender: Any) {
-        
+        TwitterAPICaller.client?.retweet(tweetId: tweetId, success: {
+            self.setRetweeted(true)
+        }, failure: { (Error) in
+            print("retweet failed \(Error)")
+        })
+    }
+    
+    func setRetweeted (_ isRetweeted:Bool){
+        if(isRetweeted){
+            retweetButton.setImage(UIImage(named: "retweet-icon-green"), for: UIControl.State.normal)
+            retweetButton.isEnabled = false
+        } else {
+            retweetButton.setImage(UIImage(named: "retweet-icon"), for: UIControl.State.normal)
+            retweetButton.isEnabled = true
+        }
     }
     
     @IBAction func likeTapped(_ sender: Any) {
-        
+        let toBeFavorited = !favorited
+        if(toBeFavorited){
+            TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(true)
+            }, failure: { (Error) in
+                print("favorite did not succeed \(Error)")
+            })
+        } else {
+            TwitterAPICaller.client?.unFavoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(false)
+            }, failure: { (Error) in
+                print("unfavorite did not succeed \(Error)")
+            })
+        }
     }
     
+    func setFavorite(_ isFavorited:Bool){
+        favorited = isFavorited
+        if(favorited){
+            likeButton.setImage(UIImage(named: "favor-icon-red"), for: UIControl.State.normal)
+        } else {
+            likeButton.setImage(UIImage(named: "favor-icon"), for: UIControl.State.normal)
+
+        }
+    }
 }
